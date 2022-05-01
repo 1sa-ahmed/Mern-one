8f8b8f010b9959dbc36cd4ea16462f8a65bc2594
ahmed <81328883+1sa-ahmed@users.noreply.github.com>
2022-05-01 14:53:53 -0400

    Newly updated

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 // Isa Ahmed assignment - 1 code
 //1. Send data to Console with template Literal
-// let names = prompt('Name plz');
-// let age = prompt('Age plz');
-// let classs = prompt('class plz');
-// console.log(
-//   `Hi ${names}, you are ${age} years old and you are in class ${classs}`
-// );
+let names = prompt('Name plz');
+let age = prompt('Age plz');
+let classs = prompt('class plz');
+console.log(
+  `Hi ${names}, you are ${age} years old and you are in class ${classs}`
+);
 //2. Make a devs info console project
-// let names = 'ahmed';
-// let Cname = 'coder IT';
-// let skill = 'js dev';
-// let loc = 'Usa';
+names = prompt('What is your name?);
+let Cname = prompt('Which company do you work?');
+let skill = prompt('What is your skill?');
+let loc = prompt('Where is that located?');
 
-// console.log(`
-//   Hi ${names}, you work at ${Cname} with a skill of ${skill} and the location of the area is ${loc}.
-// `);
+console.log(`
+  Hi ${names}, you work at ${Cname} with a skill of ${skill} and the location of the area is ${loc}.
+`);
 
 //3. Make a result marks project
-// let Sname = prompt('School name ');
-// let Cname = prompt('Certified name');
-// let roll = prompt('What is your roll');
+let Sname = prompt('School name ');
+let Cname = prompt('Certified name');
+let roll = prompt('What is your roll');
 
-// let en = Number(prompt('english marks')),
-//   es = parseInt(prompt('Spanish marks')),
-//   mh = parseFloat(prompt('Math Marks')),
-//   sc = +prompt('Scince marks'),
-//   rel = parseFloat(prompt('Religion'));
+let en = Number(prompt('english marks')),
+  es = parseInt(prompt('Spanish marks')),
+  mh = parseFloat(prompt('Math Marks')),
+  sc = +prompt('Scince marks'),
+  rel = parseFloat(prompt('Religion'));
 
-// let total = en + es + mh + sc + rel;
+let total = en + es + mh + sc + rel;
 
-// console.log(`
-//   School Name - ${Sname}
-//   Certified Name - ${Cname}
-//   Roll Num - ${roll}
+console.log(`
+  School Name - ${Sname}
+  Certified Name - ${Cname}
+  Roll Num - ${roll}
 
-//   Subject Name    Marks
-//   ----------------------
-//   English       = ${en}
-//   Spanish       = ${es}
-//   Math          = ${mh}
-//   Science       = ${sc}
-//   Religion      = ${rel}
-//   -----------------------
-//   Total Marks   = ${total}
+  Subject Name    Marks
+  ----------------------
+  English       = ${en}
+  Spanish       = ${es}
+  Math          = ${mh}
+  Science       = ${sc}
+  Religion      = ${rel}
+  -----------------------
+  Total Marks   = ${total}
 
-// `);
+`);
 //4. Make a kobol project
-// let names = prompt('What is your name?');
-// let verify = confirm('Are you ready to Marriage "' + names + '" ?');
+names = prompt('What is your name?');
+let verify = confirm('Are you ready to Marriage "' + names + '" ?');
 
-// console.log(verify);
-// console.log(`  |
-//   -> True = Yes && false = No`);
+console.log(verify);
+console.log(`  |
+  -> True = Yes && false = No`);
